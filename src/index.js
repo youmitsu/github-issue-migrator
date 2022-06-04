9aef583d9f7bd17f9005bd556dad543ec9a62a2f
Yu Mitsuhori <ymbullseye@gmail.com>
2022-06-04 13:08:53 +0900

    fix: write script code

--- src/index.js
+++ src/index.js
@@ -1,6 +1,42 @@
 "use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-const run = () => {
-    console.log('hello');
+var __awaiter = (this && this.__awaiter) || function (thisArg, _arguments, P, generator) {
+    function adopt(value) { return value instanceof P ? value : new P(function (resolve) { resolve(value); }); }
+    return new (P || (P = Promise))(function (resolve, reject) {
+        function fulfilled(value) { try { step(generator.next(value)); } catch (e) { reject(e); } }
+        function rejected(value) { try { step(generator["throw"](value)); } catch (e) { reject(e); } }
+        function step(result) { result.done ? resolve(result.value) : adopt(result.value).then(fulfilled, rejected); }
+        step((generator = generator.apply(thisArg, _arguments || [])).next());
+    });
 };
+Object.defineProperty(exports, "__esModule", { value: true });
+exports.sleep = void 0;
+const octokit_1 = require("octokit");
+const config = require('config');
+const sleep = (msec) => new Promise((resolve) => setTimeout(resolve, msec));
+exports.sleep = sleep;
+const run = () => __awaiter(void 0, void 0, void 0, function* () {
+    var _a;
+    const token = config.get('githubAccessToken');
+    const octokit = new octokit_1.Octokit({ auth: token });
+    const { data } = yield octokit.rest.issues.listForRepo({
+        owner: 'Haianh9999',
+        repo: 'Keypace',
+        page: 2,
+    });
+    let index = 0;
+    while (index <= data.length - 1) {
+        const d = data[index];
+        const title = d.title;
+        const body = (_a = d.body) !== null && _a !== void 0 ? _a : '';
+        yield (0, exports.sleep)(5000);
+        octokit.rest.issues.create({
+            owner: 'Keypace',
+            repo: 'Keypace',
+            title,
+            body,
+        });
+        console.log('created', title, body);
+        index++;
+    }
+});
 exports.default = run();
--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,34 @@
-const run = () => {
-  console.log('hello')
+import { Octokit } from 'octokit'
+
+const config = require('config')
+
+export const sleep = (msec: number): Promise<void> =>
+  new Promise((resolve) => setTimeout(resolve, msec))
+
+const run = async () => {
+  const token = config.get('githubAccessToken')
+
+  const octokit = new Octokit({ auth: token })
+  const { data } = await octokit.rest.issues.listForRepo({
+    owner: 'Haianh9999',
+    repo: 'Keypace',
+    page: 2,
+  })
+  let index = 0
+  while (index <= data.length - 1) {
+    const d = data[index]
+    const title = d.title
+    const body = d.body ?? ''
+    await sleep(5000)
+    octokit.rest.issues.create({
+      owner: 'Keypace',
+      repo: 'Keypace',
+      title,
+      body,
+    })
+    console.log('created', title, body)
+    index++
+  }
 }
 
 export default run()
